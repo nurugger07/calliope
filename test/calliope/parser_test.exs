dfb9256d45300242c7fa6c02cf2a45037aa21637
nurugger07 <dev@hashrocket.com>
2014-01-16 23:17:48 -0500

    Cleaning up parser tests

--- test/calliope/parser_test.exs
+++ test/calliope/parser_test.exs
@@ -13,23 +13,26 @@ defmodule CalliopeParserTest do
 
   @parsed_tokens [
       [ tag: "section", classes: ["container"] ],
-      [ indent: 1, tag: "h1", content: "Calliope" ],
-      [ indent: 1, tag: "h2", content: "An Elixir Haml Parser" ],
+      [ tag: "h1", indent: 1, content: "Calliope" ],
+      [ tag: "h2", indent: 1, content: "An Elixir Haml Parser" ],
       [ id: "main", indent: 1, classes: ["content"] ],
       [ indent: 2, content: "Welcome to Calliope" ]
     ]
 
   test :parse_line do
-    assert parsed_tokens(0) == parse_line tokens(0)
-    assert parsed_tokens(1) == parse_line(tokens(1))
-    assert parsed_tokens(2) == parse_line(tokens(2))
-    assert parsed_tokens(3) == parse_line(tokens(3))
-    assert parsed_tokens(4) == parse_line(tokens(4))
+    assert parsed_tokens(0) == parsed_line_tokens(tokens(0))
+    assert parsed_tokens(1) == parsed_line_tokens(tokens(1))
+    assert parsed_tokens(2) == parsed_line_tokens(tokens(2))
+    assert parsed_tokens(3) == parsed_line_tokens(tokens(3))
+    assert parsed_tokens(4) == parsed_line_tokens(tokens(4))
   end
 
   defp tokens(n), do: line(@tokens, n)
 
-  defp parsed_tokens(n), do: line(@parsed_tokens, n)
+  defp parsed_tokens(n), do: Enum.sort line(@parsed_tokens, n)
+
+  defp parsed_line_tokens(tokens), do: Enum.sort parse_line(tokens)
 
   defp line(list, n), do: Enum.fetch!(list, n)
+
 end
