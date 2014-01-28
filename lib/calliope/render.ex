048e7114e1233b2072cf9bb34c8ed6e128c17483
Hashrocket Workstation <dev@hashrocket.com>
2014-01-28 01:22:56 +0000

    Test render haml to html

--- lib/calliope/render.ex
+++ lib/calliope/render.ex
@@ -1,3 +1,9 @@
 defmodule Calliope.Render do
 
+  import Calliope.Tokenizer
+  import Calliope.Parser
+  import Calliope.Compiler
+
+  def render(haml), do: tokenize(haml) |> parse |> compile
+
 end
--- test/calliope/render_test.exs
+++ test/calliope/render_test.exs
@@ -1,4 +1,27 @@
 defmodule CalliopeRenderTest do
   use ExUnit.Case
 
+  import Calliope.Render
+
+  @haml %s{
+%section.container
+	%h1 Calliope
+	%h2 An Elixir Haml Parser
+	#main.content
+		Welcome to Calliope}
+
+  @html Regex.replace(%r/(^\s*)|(\s+$)|(\n)/m, %s{
+    <section class="container">
+      <h1>Calliope</h1>
+      <h2>An Elixir Haml Parser</h2>
+      <div id="main" class="content">
+        Welcome to Calliope
+      </div>
+    </section>
+  }, "")
+
+  test :render do
+    assert @html == render @haml
+  end
+
 end
