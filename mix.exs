c30a410f3d25b11648107a6b1de0ddf772868483
nurugger07 <dev@hashrocket.com>
2014-02-12 09:22:11 -0500

    Bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,18 +3,15 @@ defmodule Calliope.Mixfile do
 
   def project do
     [ app: :calliope,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 0.12.0",
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     []
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     []
   end
