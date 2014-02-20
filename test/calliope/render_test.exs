713ca67c3c0c0b48e1284a565e3d9823e424fcb3
Johnny Winn <dev+nurugger07@hashrocket.com>
2014-02-20 13:16:04 -0500

    Clean up render test

--- test/calliope/render_test.exs
+++ test/calliope/render_test.exs
@@ -30,33 +30,6 @@ defmodule CalliopeRenderTest do
 
   @haml_with_args "%a{href: url}= title"
 
-  @posts [
-    { 1, "One Great Article!", "This is some great content." },
-    { 2, "Another Great Article", "These articles just keep coming." }
-  ]
-
-  @haml_with_collection """
-- lc { id, subject, content } inlist posts do
-  %article.post
-    %h1
-      %a{href: "/posts/\#{id}"}= subject
-    = content
-  """
-
-  @html_with_collection Regex.replace(~r/(^\s*)|(\s+$)|(\n)/m, ~s{
-  <article class="post">
-    <h1><a href="/posts/1">One Great Article!</a></h1>
-    <p>
-      This is some great content.
-    </p>
-  </article>
-  <article class="post">
-    <h1><a href="/posts/2">Another Great Article</a></h1>
-    <p>
-      These articles just keep coming.
-    </p>
-  </article>}, "")
-
   test :render do
     assert @html == render @haml, [title: "Calliope"]
     assert "<h1>This is Calliope</h1>" == render "%h1 This is \#{title}", [title: "Calliope"]
@@ -67,7 +40,8 @@ defmodule CalliopeRenderTest do
       render @haml_with_args, [ url: "http://google.com", title: "Google" ]
   end
 
-  # test :render_with_collection do
-  #   assert @html_with_collection == render @haml_with_collection, [posts: @posts]
-  # end
+  test :render_with_collection do
+    file = File.read!("test/fixtures/list_comprehension_example.haml")
+    assert "<div>post 1</div><div>post 2</div>" == render file, [posts: [{ 1, "post 1" }, { 2, "post 2" }]]
+  end
 end
