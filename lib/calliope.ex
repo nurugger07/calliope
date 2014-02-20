72c8c6a4c7caa1f75052d71e8add7e8f3cf0859f
Hashrocket Workstation <dev@hashrocket.com>
2014-02-20 12:32:06 -0500

    Include using macro

--- lib/calliope.ex
+++ lib/calliope.ex
@@ -1,2 +1,11 @@
 defmodule Calliope do
+  use Calliope.Render
+
+  defmacro __using__([]) do
+    quote do
+      import unquote __MODULE__
+
+      use Calliope.Render
+    end
+  end
 end
--- lib/calliope/render.ex
+++ lib/calliope/render.ex
@@ -1,9 +1,13 @@
 defmodule Calliope.Render do
 
-  import Calliope.Tokenizer
-  import Calliope.Parser
-  import Calliope.Compiler
+  defmacro __using__([]) do
+    quote do
+      import unquote __MODULE__
+      import Calliope.Tokenizer
+      import Calliope.Parser
+      import Calliope.Compiler
 
-  def render(haml, args\\[]), do: tokenize(haml) |> parse |> compile(args)
-
+      def render(haml, args\\[]), do: tokenize(haml) |> parse |> compile(args)
+    end
+  end
 end
