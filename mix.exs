18fac545cd2f8604ba56f79c3ad4f67446001e6a
Hashrocket Workstation <dev@hashrocket.com>
2014-05-27 12:46:25 -0400

    Add hex package support

--- mix.exs
+++ mix.exs
@@ -1,10 +1,22 @@
+Code.ensure_loaded?(Hex) and Hex.start
+
 defmodule Calliope.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :calliope,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: ">= 0.13.3",
-      deps: [] ]
+      deps: [],
+      package: [
+        files: ["lib", "mix.exs", "README*", "LICENSE*"],
+        contributors: ["Johnny Winn"],
+        licenses: ["Apache 2.0"],
+        links: [ github: "https://github.com/nurugger07/calliope" ] 
+      ],
+      description: """
+      An Elixir library for parsing haml templates.
+      """
+    ]
   end
 end
