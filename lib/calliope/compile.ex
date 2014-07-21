5c04c17719a13a36f1fe7cbd9bbc127ca0f27713
Johnny Winn <dev+nurugger07@traitify.com>
2014-07-20 23:50:55 -0400

    Revert changes to compiled strings

--- lib/calliope/compile.ex
+++ lib/calliope/compile.ex
@@ -6,14 +6,14 @@ defmodule Calliope.Compiler do
   @lc ~r/^(.*)do:?(.*)$/
 
   @doctypes [
-    { :"!!!", ~s'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' },
-    { :"!!! 5", ~s'<!DOCTYPE html>' },
-    { :"!!! Strict", ~s'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' },
-    { :"!!! Frameset", ~s'<!DOCTYPE html PUBLIC "W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">' },
-    { :"!!! 1.1", ~s'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">' },
-    { :"!!! Basic", ~s'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">' },
-    { :"!!! Mobile", ~s'<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">' },
-    { :"!!! RDFa", ~s'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">' }
+    { :"!!!", "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"},
+    { :"!!! 5", "<!DOCTYPE html>" },
+    { :"!!! Strict", "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" },
+    { :"!!! Frameset", "<!DOCTYPE html PUBLIC \"W3C//DTD XHTML 1.0 Frameset//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">" },
+    { :"!!! 1.1", "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">" },
+    { :"!!! Basic", "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML Basic 1.1//EN\" \"http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd\">" },
+    { :"!!! Mobile", "<!DOCTYPE html PUBLIC \"-//WAPFORUM//DTD XHTML Mobile 1.2//EN\" \"http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd\">" },
+    { :"!!! RDFa", "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML+RDFa 1.0//EN\" \"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd\">" }
   ]
 
   def compile([]), do: ""
@@ -112,8 +112,9 @@ defmodule Calliope.Compiler do
   def reject_or_compile_key(key, value), do: compile_key({ key, value })
 
   def compile_key({ :attributes, value }), do: " #{value}"
-  def compile_key({ :classes, value}), do: ~s' class="#{Enum.join(value, " ")}"'
-  def compile_key({ :id, value }), do: ~s' id="#{value}"'
+  def compile_key({ :classes, value}), do: " class=\"#{Enum.join(value, " ")}\""
+
+  def compile_key({ :id, value }), do: " id=\"#{value}\""
 
   def tag(node) do
     cond do
