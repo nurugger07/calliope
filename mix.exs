0df1bbea3eadc2b953a6bcffdd7e218045c09335
Johnny Winn <johnny+nurugger07@traitify.com>
2014-09-13 16:20:29 -0700

    Update version for v1.0.0

--- mix.exs
+++ mix.exs
@@ -5,14 +5,14 @@ defmodule Calliope.Mixfile do
 
   def project do
     [ app: :calliope,
-      version: "0.2.5",
-      elixir: ">= 0.15.0",
+      version: "0.2.6",
+      elixir: ">= 1.0.0",
       deps: [],
       package: [
         files: ["lib", "mix.exs", "README*", "LICENSE*"],
         contributors: ["Johnny Winn"],
         licenses: ["Apache 2.0"],
-        links: [ github: "https://github.com/nurugger07/calliope" ] 
+        links: [ github: "https://github.com/nurugger07/calliope" ]
       ],
       description: """
       An Elixir library for parsing haml templates.
