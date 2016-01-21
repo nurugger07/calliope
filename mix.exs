2a9b0e3a9ce646accee12984a568f4282bdac780
Stephen M. Pallen <smpallen99@yahoo.com>
2016-01-21 17:25:08 -0500

    update version

--- mix.exs
+++ mix.exs
@@ -5,12 +5,12 @@ defmodule Calliope.Mixfile do
 
   def project do
     [ app: :calliope,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: ">= 1.0.0",
       deps: [],
       package: [
         files: ["lib", "mix.exs", "README*", "LICENSE*"],
-        contributors: ["Johnny Winn"],
+        contributors: ["Johnny Winn", "Stephen Pallen"],
         licenses: ["Apache 2.0"],
         links: %{ "Github" => "https://github.com/nurugger07/calliope" }
       ],
